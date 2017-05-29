37ffa3a6baa6273e03c69db6d01ef30f17de21b9
sevenLee <seven.lee@infinitus-int.com>
2017-05-29 08:40:25 +0800

    - add superagent

--- config/global.base.js
+++ config/global.base.js
@@ -1,6 +1,7 @@
 module.exports = {
         REQUEST_TIMEOUT: 10000,
         expressPort: 3000,
-        API_HOST: 'http://localhost:3000'
+        API_HOST: 'http://www.discograms.com'
+        //API_HOST: 'http://localhost:3000'
 
 }
